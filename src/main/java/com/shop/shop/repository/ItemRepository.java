edec5d267fb1697db8cfbe9a92447ad343be2c5c
mango <esudatcom@gmail.com>
2023-09-29 22:32:18 +0900

    Practice Query Method(find문법)

--- src/main/java/com/shop/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/shop/repository/ItemRepository.java
@@ -3,7 +3,18 @@ package com.shop.shop.repository;
 import com.shop.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> { // <Entity Type Class, PK Type>
+    // 상품의 이름을 이용하여 데이터를 조회하기 위해 findByItemNm 메소드 추가하기
+    List<Item> findByItemNm(String itemNm); // itemNm(상품명) 으로 데이터를 조회하기 위해서 By 뒤에 필드명인 ItemNm을 메소드의 이름으로 붙이기
 
+    // 상품명과 상품 상세 설명을 OR 조건을 이용하여 조회하는 쿼리 메소드
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
+    // 파라미터로 넘어온 price 변수 보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThan(Integer price);
+
+    // 상품의 가격을 내림차순으로 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer Price);
 }
--- src/test/java/com/shop/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,8 +22,8 @@ class ItemRepositoryTest {
 
     @Test // 이 메소드 테스트 대상으로 지정하기
     @DisplayName("상품 저장 테스트") // 테스트 코드 실행 시, 여기에 지정한 테스트명 노출
-    public void createItemTest(){
-        Item item = new Item() ;
+    public void createItemTest() {
+        Item item = new Item();
         item.setItemNm("테스트 상품");
         item.setPrice(10000);
         item.setItemDetail("테스트 상품 상세 설명");
@@ -32,5 +33,61 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    // 테스트 코드 실행 시 데이터베이스에 상품 데이터가 없으므로 테스트 데이터 생성을 위해서 10개의 상품을 저장하는 메소드
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1"); // 리파지토리 인터페이스에서 작성했던 findByItemNm 메소드 호출
+        for (Item item : itemList) {
+            System.out.println(item.toString()); // 조회 결과 얻은 item 객체들을 출력하기
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 OR 테스트")
+    void findByItemNmOrItemDetail() {
+        this.createItemList();
+        // 상품명이 "테스트 상품1" 또는 상세 설명이 "테스트 상품 상세 설명5"이면 해당 상품을 itemList에 할당
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    void findByPriceLessThan() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
